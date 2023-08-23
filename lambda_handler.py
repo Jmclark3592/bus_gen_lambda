bd526899b240724c128bcbde6f2a84a03e3dec63
Jmclark3592 <jmclark3592@gmail.com>
2023-08-23 10:19:27 -0500

    added prompt

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,9 +1,10 @@
 import json
 import requests
 import os
+import boto3
 
-# using lambda env variables (under config tab)
 OPENAI_KEY = os.environ["OPENAI_KEY"]
+ses_client = boto3.client("ses", region_name="us-east-2")
 
 
 def lambda_handler(event, context):
@@ -15,8 +16,14 @@ def lambda_handler(event, context):
     email = json_data.get("email")
     web_content = json_data.get("web_content")
 
-    # Use the necessary prompts and instructions for ChatGPT
-    chatgpt_prompt = f"Compose an email for {business_name} based on the following web content:\n\n{web_content}."
+    # Use the new explicit prompt for ChatGPT
+    chatgpt_prompt = (
+        f"Craft a professional email for {business_name} where we offer our services. "
+        f"We specialize in helping businesses leverage AI to redefine their strategy. "
+        f"Highlight the advantages of integrating AI into their existing operations, "
+        f"and how it can revolutionize their business model. Use the following content for reference:\n\n{web_content}"
+    )
+
     chatgpt_url = "https://api.openai.com/v1/engines/davinci/completions"
 
     headers = {
@@ -26,18 +33,47 @@ def lambda_handler(event, context):
 
     payload = {
         "prompt": chatgpt_prompt,
-        "temperature": 0.7,
+        "temperature": 0.5,
         "max_tokens": 500,
-    }  # max tokens 500 about 125 words
+    }
 
     response = requests.post(chatgpt_url, json=payload, headers=headers)
     chatgpt_response = response.json()
-    composed_email = chatgpt_response["choices"][0]["text"]
 
-    # TODO: Trigger the email sending process using Zapier or another Lambda function
-    # For now, let's just print the composed email
-    print("Composed Email:")
-    print(composed_email)
+    # Print the entire ChatGPT response for debugging
+    print("ChatGPT Response:")
+    print(json.dumps(chatgpt_response, indent=2))
+
+    # Attempt to retrieve composed email text
+    try:
+        composed_email = chatgpt_response["choices"][0]["text"]
+        print("Composed Email:")
+        print(composed_email)
+
+        # Send email using SES
+        response = ses_client.send_email(
+            Source="justin@gopurple.ai",  # Replace with your verified email
+            Destination={
+                "ToAddresses": [
+                    email,  # The business's email address
+                ],
+            },
+            Message={
+                "Subject": {
+                    "Data": "Discover the Power of AI for Your Business",
+                    "Charset": "UTF-8",
+                },
+                "Body": {
+                    "Text": {
+                        "Data": composed_email,
+                        "Charset": "UTF-8",
+                    },
+                },
+            },
+        )
+
+    except KeyError as e:
+        print("Error retrieving composed email:", e)
 
     return {
         "statusCode": 200,
