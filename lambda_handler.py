6aa8aafd3416031b9cbd690abac605e02d82d2a6
Jmclark3592 <jmclark3592@gmail.com>
2023-08-23 13:39:55 -0500

    mailchimp doesnt work, see comments

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,83 +1,81 @@
+# Mailchimp not working, edit campaign
+# To section incomplete- MC is mail lists, can't send one email and not bug them again.
+# Content section might be wrong, |DYNAMIC_CONTENT| does not appear to be a valid mergefield
+
 import json
 import requests
 import os
-import boto3
 
 OPENAI_KEY = os.environ["OPENAI_KEY"]
-ses_client = boto3.client("ses", region_name="us-east-2")
+MAILCHIMP_API_KEY = os.environ["MAILCHIMP_API_KEY"]
+MAILCHIMP_LIST_ID = os.environ["MAILCHIMP_LIST_ID"]
+MAILCHIMP_DATA_CENTER = "us12"
+MAILCHIMP_CAMPAIGN_ID = "1084706"
 
 
 def lambda_handler(event, context):
-    # Retrieve JSON data from the event
+    # Extract company info from the SQS message
     json_data = json.loads(event["Records"][0]["body"])
-
-    # Extract necessary information from the JSON data (business name, email, web content)
     business_name = json_data.get("name")
     email = json_data.get("email")
     web_content = json_data.get("web_content")
 
-    # Use the new explicit prompt for ChatGPT
-    chatgpt_prompt = (
-        f"Craft a professional email for {business_name} where we offer our services. "
-        f"We specialize in helping businesses leverage AI to redefine their strategy. "
-        f"Highlight the advantages of integrating AI into their existing operations, "
-        f"and how it can revolutionize their business model. Use the following content for reference:\n\n{web_content}"
-    )
-
-    chatgpt_url = "https://api.openai.com/v1/engines/davinci/completions"
-
-    headers = {
+    # Generate email content using ChatGPT
+    chatgpt_url = "https://api.openai.com/v1/chat/completions"
+    headers_openai = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {OPENAI_KEY}",
     }
-
+    chatgpt_prompt = f"You work for Purple AI, a company that offers AI solutions to businesses. Craft a professional email offering our services to {business_name} using their web content: {web_content}"
     payload = {
-        "prompt": chatgpt_prompt,
-        "temperature": 0.5,
-        "max_tokens": 500,
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": chatgpt_prompt},
+        ],
     }
 
-    response = requests.post(chatgpt_url, json=payload, headers=headers)
-    chatgpt_response = response.json()
+    response = requests.post(chatgpt_url, json=payload, headers=headers_openai)
+    openai_response = response.json()
+    print("OpenAI Response:", openai_response)
 
-    # Print the entire ChatGPT response for debugging
-    print("ChatGPT Response:")
-    print(json.dumps(chatgpt_response, indent=2))
-
-    # Attempt to retrieve composed email text
     try:
-        composed_email = chatgpt_response["choices"][0]["text"]
-        print("Composed Email:")
-        print(composed_email)
+        chatgpt_content = openai_response["choices"][0]["message"]["content"]
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps("Failed to generate content from OpenAI"),
+        }
 
-        # Send email using SES
-        response = ses_client.send_email(
-            Source="justin@gopurple.ai",  # Replace with your verified email
-            Destination={
-                "ToAddresses": [
-                    email,  # The business's email address
-                ],
-            },
-            Message={
-                "Subject": {
-                    "Data": "Discover the Power of AI for Your Business",
-                    "Charset": "UTF-8",
-                },
-                "Body": {
-                    "Text": {
-                        "Data": composed_email,
-                        "Charset": "UTF-8",
-                    },
-                },
-            },
-        )
+    print("Preparing to call Mailchimp with content:", chatgpt_content)
 
-    except KeyError as e:
-        print("Error retrieving composed email:", e)
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            "Email composition and sending process triggered successfully"
-        ),
+    # Mailchimp API call to add the contact
+    mailchimp_url = f"https://{MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0/lists/{MAILCHIMP_LIST_ID}/members"
+    headers_mailchimp = {
+        "Authorization": f"Bearer {MAILCHIMP_API_KEY}",
+        "Content-Type": "application/json",
     }
+    payload_mailchimp = {
+        "email_address": email,
+        "status": "subscribed",
+        "merge_fields": {"DYNAMIC_CONTENT": chatgpt_content},
+    }
+
+    response = requests.post(
+        mailchimp_url, json=payload_mailchimp, headers=headers_mailchimp
+    )
+    mailchimp_response = response.json()
+
+    # Check the Mailchimp response for success or errors
+    if response.status_code == 200:
+        return {
+            "statusCode": 200,
+            "body": "Email content generated and contact added to Mailchimp.",
+        }
+    else:
+        # Log the Mailchimp error for debugging
+        print("Mailchimp Response:", mailchimp_response)
+        return {
+            "statusCode": 400,
+            "body": f"Failed to add/update contact in Mailchimp: {mailchimp_response.get('detail', 'Unknown error')}",
+        }
