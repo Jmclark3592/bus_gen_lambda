aeb2ae2a18da916660d0ca7a7a64734a3464f7eb
Jmclark3592 <jmclark3592@gmail.com>
2023-08-31 15:32:53 -0500

    upd with mandrill and html template, rmv chatgpt

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,90 +1,44 @@
-# Mailchimp not working, edit campaign
-# To section incomplete- MC is mail lists, can't send one email and not bug them again.
-# Content section might be wrong, |DYNAMIC_CONTENT| does not appear to be a valid mergefield
-
 import json
 import requests
 import os
 
-OPENAI_KEY = os.environ["OPENAI_KEY"]
-MANDRILL_API_KEY = os.environ["MANDRILL_API_KEY"]
+def send_email(event, context):
+    # Extracting details from the SQS event
+    sqs_message = event['Records'][0]['body']
+    sqs_data = json.loads(sqs_message)
+    email = sqs_data.get('email', '')
+    name = sqs_data.get('name', '')
 
+    # Mailchimp Transactional API configurations
+    MANDRILL_API_KEY = os.environ.get('MANDRILL_API_KEY')
+    API_URL = 'https://mandrillapp.com/api/1.0/messages/send-template.json'
+    TEMPLATE_NAME = 'survey2'
 
-def lambda_handler(event, context):
-    # Extract company info from the SQS message
-    json_data = json.loads(event["Records"][0]["body"])
-    business_name = json_data.get("name")
-    email = json_data.get("email")
-    web_content = json_data.get("web_content")
-
-    # Generate email content using ChatGPT
-    chatgpt_url = "https://api.openai.com/v1/chat/completions"
-    headers_openai = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {OPENAI_KEY}",
-    }
-    chatgpt_prompt = (
-        f"Hello Assistant, I want to provide you with some context first. I am Brooks, owner of Purple AI. Our company specializes in:"
-        f"\n- Expert training in AI technologies."
-        f"\n- Strategic business insights through AI."
-        f"\n- Driving product and process innovation using AI."
-        f"\n\nOur mission is to integrate AI technologies into business workflows, redefine strategies, and foster innovation. "
-        f"\n\nNow, I've come across a company named {business_name}. Here's some information about them from their website: \n\n{web_content}"
-        f"\n\nBased on this information about {business_name}, can you help me craft a concise email offering Purple AI's services to them, highlighting how we can benefit them specifically?"
-        f"\n\nPlease address the email to the {business_name} team."
-    )
-
+    # Constructing the API payload
     payload = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": chatgpt_prompt},
-        ],
-    }
-
-    response = requests.post(chatgpt_url, json=payload, headers=headers_openai)
-    openai_response = response.json()
-    print("OpenAI Response:", openai_response)
-
-    try:
-        chatgpt_content = openai_response["choices"][0]["message"]["content"]
-    except KeyError:
-        return {
-            "statusCode": 400,
-            "body": json.dumps("Failed to generate content from OpenAI"),
-        }
-
-    # Mandrill API call to send the email
-    mandrill_url = "https://mandrillapp.com/api/1.0/messages/send.json"
-    headers_mandrill = {
-        "Content-Type": "application/json",
-    }
-    payload_mandrill = {
         "key": MANDRILL_API_KEY,
+        "template_name": TEMPLATE_NAME,
+        "template_content": [],
         "message": {
-            "html": chatgpt_content,
+            "from_email": "brooks@gopurple.ai",
+            "from_name": "Brooks",
+            "to": [
+                {
+                    "email": email,
+                    "name": name,
+                    "type": "to"
+                }
+            ],
             "subject": "Discover the Power of AI for Your Business",
-            "from_email": "brooks@gopurple.ai",  # sender address
-            "from_name": "Brooks from Purple AI",
-            "to": [{"email": email, "type": "to"}],
-        },
+            "global_merge_vars": [
+                {
+                    "name": "BUSINESSNAME",
+                    "content": name
+                }
+            ]
+        }
     }
 
-    response = requests.post(
-        mandrill_url, json=payload_mandrill, headers=headers_mandrill
-    )
-    mandrill_response = response.json()
-
-    # Check the Mandrill response for success or errors
-    if mandrill_response[0].get("status") == "sent":
-        return {
-            "statusCode": 200,
-            "body": "Email content generated and sent via Mandrill.",
-        }
-    else:
-        # Log the Mandrill error for debugging
-        print("Mandrill Response:", mandrill_response)
-        return {
-            "statusCode": 400,
-            "body": f"Failed to send email via Mandrill: {mandrill_response[0].get('reject_reason', 'Unknown error')}",
-        }
+    # Sending the email
+    response = requests.post(API_URL, data=json.dumps(payload))
+    return response.json()
